9e30b110d65d1a4354500d944975c776786a9b9f
Beena Nailwal <beenanailwal1@gmail.com>
2026-04-26 10:31:48 +0530

    Delete validators directory

--- validators/taskValidator.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-export const createTaskSchema = Joi.object({
-    title: Joi.string().required().messages({
-        "string.empty": "Title is required"
-    }),
-    description: Joi.string().allow("")
-})
